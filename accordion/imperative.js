c0c5de12b1df9a16b119ae42c27956ca6214327b
Ilya Obolenskiy <iobolenskiy@avito.ru>
2023-03-28 22:18:38 +0400

    Added files work practice

---
+++ accordion/imperative.js
@@ -0,0 +1,10 @@
+// TODO: implement
+/*
+1. Create an Accordion function which receives domElement
+2. Initialize element and add 'accordion' class
+3. Add 'accordion__header' and 'accordion__body'
+    to corresponding elements within a domElement
+4. Add 'click' event listener to a domElement.
+    Copy from 'imperative.js' and adjust/fix
+5. Create 'this.expand' and 'this.collapse' function properties and corresponding logic
+ */
