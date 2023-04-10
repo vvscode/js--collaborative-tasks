89c1ec548db27ec712409504c8a5f835b1f32665
eubash <nemezidaxxx@mail.ru>
2023-04-10 12:32:23 +0200

    fix: code transformer

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  transform: {
+    "\\.[jt]sx?$": "babel-jest",
+  },
 };
