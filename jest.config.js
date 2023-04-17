47b578fea76009aedce731fc0c649336d7f452c8
eubash <nemezidaxxx@mail.ru>
2023-04-17 16:32:13 +0200

    feat: add template task

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  transform: {
-    "\\.[jt]sx?$": "babel-jest",
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
