096dc8f85439a1879fd464b855357bf25daa9e51
Andrey Osipuk <andrey.o@list.ru>
2023-04-27 20:57:56 +0400

    fix

--- src/createCalculateUI.test.js
+++ src/createCalculateUI.test.js
@@ -1,5 +1,7 @@
 import { createCalculateUI } from "./createCalculateUI";
 
 describe("createCalculateUI", () => {
-
+  it("is a functions", () => {
+    expect(createCalculateUI).toBeInstanceOf(Function);
+  });
 });
